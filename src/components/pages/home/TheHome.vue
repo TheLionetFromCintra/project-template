f3f3b60143b4469d30d562133bcfebd47b545600
theLionetFromCintra <vinipyx34@bk.ru>
2022-12-25 22:17:33 +0300

    added observer

--- src/components/pages/home/TheHome.vue
+++ src/components/pages/home/TheHome.vue
@@ -1,29 +1,54 @@
 <template>
   <the-header></the-header>
   <main-block></main-block>
-  <base-wrapper class="steps" title="Как это работает?">
+
+  <transition name="section" mode="out-in">
+    <base-wrapper
+      class="steps"
+      title="Как это работает?"
+      v-if="sections[0].isVisible"
+    >
       <template #default>
         <steps-list></steps-list>
       </template>
-  </base-wrapper>
-  <base-wrapper class="ways" title="Способы получения займа в МФО">
-    <template #default>
-      <p class="text">Моментальный подбор подходящих займов: просто укажите, какая сумма вам необходима. Мы подберем предложения с моментальным переводом денег на карту.</p>
-      <pay-list></pay-list>
-    </template>
-  </base-wrapper>
-  <div class="bg">
-    <base-wrapper class="about" title="О нас">
+    </base-wrapper>
+  </transition>
+  <transition name="section" mode="out-in">
+    <base-wrapper
+      class="ways"
+      title="Способы получения займа в МФО"
+      v-if="sections[1].isVisible"
+    >
       <template #default>
-        <about-list></about-list>
+        <p class="text">
+          Моментальный подбор подходящих займов: просто укажите, какая сумма вам
+          необходима. Мы подберем предложения с моментальным переводом денег на
+          карту.
+        </p>
+        <pay-list></pay-list>
       </template>
     </base-wrapper>
-  </div>
-  <base-wrapper class="plans" title="Выберите свой займ в МФО">
-    <template #default>
-      123
-    </template>
-  </base-wrapper>
+  </transition>
+  <transition name="section" mode="out-in">
+    <div class="bg" v-if="sections[2].isVisible">
+      <base-wrapper class="about" title="О нас">
+        <template #default>
+          <about-list></about-list>
+        </template>
+      </base-wrapper>
+    </div>
+  </transition>
+  <transition name="section" mode="out-in">
+    <base-wrapper
+      class="plans"
+      title="Выберите свой займ в МФО"
+      v-if="sections[3].isVisible"
+    >
+      <template #default> 123 </template>
+    </base-wrapper>
+  </transition>
+
+  <the-observer @intersect="observeSection"></the-observer>
 </template>
 
 <script>
@@ -32,6 +57,7 @@ import MainBlock from "@/components/pages/home/layouts/MainBlock";
 import StepsList from "@/components/pages/home/components/StepsList";
 import PayList from "@/components/pages/home/components/PayList";
 import AboutList from "@/components/pages/home/components/AboutList";
+import TheObserver from "./components/TheObserver.vue";
 
 export default {
   components: {
@@ -39,74 +65,81 @@ export default {
     PayList,
     StepsList,
     TheHeader,
-    MainBlock
+    MainBlock,
+    TheObserver,
   },
   data() {
     return {
-      scrolled: false
+      counter: 0,
+      maxSteps: 4,
+      sections: [
+        {
+          isVisible: false,
+        },
+        {
+          isVisible: false,
+        },
+        {
+          isVisible: false,
+        },
+        {
+          isVisible: false,
+        },
+      ],
     };
   },
   methods: {
-
-  },
-  mounted() {
-    const allSections = document.querySelectorAll('section');
-
-    const revealSection = function (entries, observer) {
-      const [entry] = entries;
-
-      if (!entry.isIntersecting) return;
-
-      entry.target.classList.remove('section--hidden');
-      observer.unobserve(entry.target);
-    };
-
-    const sectionObserver = new IntersectionObserver(revealSection, {
-      root: null,
-      threshold: 0.15,
-    });
-
-    allSections.forEach(function (section) {
-      sectionObserver.observe(section);
-      section.classList.add('section--hidden');
-    });
+    observeSection() {
+      if (this.counter < this.maxSteps) {
+        this.sections[this.counter].isVisible = true;
+        this.counter++;
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  section {
-    transition: transform 1s, opacity 1s;
+.steps {
+  margin-top: 70px;
+}
+.ways {
+  margin-top: 98px;
+  padding-bottom: 76px;
+  .text {
+    max-width: 1028px;
+    margin: 0 auto 48px auto;
+    font-size: 24px;
+    line-height: 160.9%;
+    text-align: center;
+    color: $primary-black-dark;
   }
-
-  .section--hidden {
-    opacity: 0;
-    transform: translateY(8rem);
-  }
-  .steps {
-    margin-top: 70px;
-  }
-  .ways {
-    margin-top: 98px;
-    padding-bottom: 76px;
-    .text {
-      max-width: 1028px;
-      margin: 0 auto 48px auto;
-      font-size: 24px;
-      line-height: 160.9%;
-      text-align: center;
-      color: $primary-black-dark;
-    }
-  }
-  .about {
-    padding: 48px 0 50px 0;
-  }
-  .plans {
-    margin-top: 86px;
-  }
-  .bg {
-    overflow: hidden;
-    background: #EFF3F9 url('@/assets/images/test-bg.svg') repeat 1700px 920px;
-    //background: #EFF3F9 url('@/assets/images/test-bg.svg') repeat 500px 500px;
-  }
+}
+.about {
+  padding: 48px 0 50px 0;
+}
+.plans {
+  margin-top: 86px;
+}
+.bg {
+  overflow: hidden;
+  background: #eff3f9 url("@/assets/images/test-bg.svg") repeat 1700px 920px;
+  //background: #EFF3F9 url('@/assets/images/test-bg.svg') repeat 500px 500px;
+}
+.section-enter-from,
+.section-leave-to {
+  opacity: 0;
+  transform: translateY(8rem);
+}
+.section-enter-active {
+  transition: opacity 1s ease-out, transform 1.5s ease-out;
+}
+.section-leave-active {
+  transition: opacity 1s ease-in, transform 1.5s ease-in;
+}
+.section-enter-to,
+.section-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
 </style>
--- src/components/ui/base/BaseWrapper.vue
+++ src/components/ui/base/BaseWrapper.vue
@@ -10,44 +10,48 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      title: {
-        type: String,
-        required: true,
-      }
-    }
-  }
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  h2 {
-    margin-bottom: 72px;
-  }
+h2 {
+  margin-bottom: 72px;
+}
+.wrapper {
+  width: 100%;
+  margin: 0 auto;
+}
+.steps {
   .wrapper {
-    width: 100%;
-    margin: 0 auto;
+    max-width: 908px;
   }
-  .steps {
-    .wrapper {
-      max-width: 908px;
-    }
+}
+.ways {
+  .wrapper {
+    box-shadow: 0px 232px 93px rgba(205, 207, 210, 0.01),
+      0px 131px 78px rgba(205, 207, 210, 0.05),
+      0px 58px 58px rgba(205, 207, 210, 0.09),
+      0px 15px 30px rgba(180, 185, 204, 0.08),
+      0px 0px 0px rgba(135, 135, 135, 0.42);
+    border-radius: 10px;
+    padding: 38px 48px 74px 48px;
   }
-  .ways {
-    .wrapper {
-      box-shadow: 0px 232px 93px rgba(205, 207, 210, 0.01), 0px 131px 78px rgba(205, 207, 210, 0.05), 0px 58px 58px rgba(205, 207, 210, 0.09), 0px 15px 30px rgba(180, 185, 204, 0.08), 0px 0px 0px rgba(135, 135, 135, 0.42);
-      border-radius: 10px;
-      padding: 38px 48px 74px 48px;
-    }
+}
+.about {
+  .wrapper {
+    max-width: 824px;
   }
-  .about {
-    .wrapper {
-      max-width: 824px;
-    }
+}
+.plans {
+  .wrapper {
+    max-width: 898px;
   }
-  .plans {
-    .wrapper {
-      max-width: 898px;
-    }
-  }
+}
 </style>
